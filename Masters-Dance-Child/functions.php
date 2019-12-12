cde2b5011d081ed2b4c4382e10b2702f49634710
James Burrell <james@kakemultimedia.com>
2019-12-12 01:28:30 -0600

    Added functionality to site.js

--- Masters-Dance-Child/functions.php
+++ Masters-Dance-Child/functions.php
@@ -1,9 +1,16 @@
 <?php
-	/* Child Theme Scripts */
+	/* Child Theme Styles */
 	function theme_enqueue_styles() {
 		wp_enqueue_style( 'kake-parent-stylesheet', get_template_directory_uri() . '/style.css' );
 	}
 	add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+	/* Child Theme Scripts */
+	function theme_enqueue_scripts() {
+		wp_deregister_script( 'jquery' );
+		wp_register_script( 'jquery', '//code.jquery.com/jquery-2.2.4.min.js', false, null );
+		wp_enqueue_script( 'sitejs', get_stylesheet_directory_uri() . '/assets/js/site.js', array( 'jquery' ), null, true );
+	}
+	add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts', 100, 1 );
 
 	/* Custom Fonts List */
 	$font_list = array(
@@ -22,3 +29,10 @@
 	 **/
 	include_once('includes/cpts/entries/entries-cpt.php');
 	include_once('includes/cpts/characters/characters-cpt.php');
+
+	/* Remove Custom Post Types */
+	function delete_post_type(){
+		unregister_post_type('testimonials');
+		unregister_post_type('template');
+	}
+	add_action( 'init', 'delete_post_type' );
