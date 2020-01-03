3cb1f79a15b28920f61301eae9c40e08df7d8ed9
James Burrell <james@kakemultimedia.com>
2020-01-03 10:18:49 -0600

    Overall updates to theme

--- Masters-Dance-Child/includes/cpts/entries/includes/shortcodes.php
+++ Masters-Dance-Child/includes/cpts/entries/includes/shortcodes.php
@@ -16,7 +16,43 @@
         ob_start();
         ?>
 
-                        
+            <div class="container-fluid" id="list-container">   
+                <div class="row character-grid" id="list-row">
+                    
+                    <?php
+                        $args = array(
+                            'post_type' => 'character',
+                            'nopaging' => true,
+                            'posts_per_page' => -1,
+                            'order' => $atts['order'],
+                            'orderby' => $atts['orderby'],
+                        );
+                        $query = new WP_Query( $args );
+                        if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post();
+
+                        // Variables
+                        global $post;
+                        $attachment_id = get_post_thumbnail_id();
+                        $character_img = wp_get_attachment_image_src( $attachment_id, 'full', false );
+                        $slug = $post->post_name;
+                        $canon_debut = get_the_terms( $post->ID, 'canon_debut_tax' );
+                        $realtime_debut = get_the_terms( $post->ID, 'realtime_debut_tax' );
+                    ?>
+
+                        <div class="carousel-inner" id="entry-list-container">
+
+                            <div class="carousel-item">
+
+                            <!-- end .carousel-item --></div>
+
+                        <!-- end .carousel-inner --></div>
+
+                    <?php endwhile; wp_reset_postdata(); else : ?>
+                        <div class="col-lg-12" id="portfolio-well">
+                            <p><?php esc_html_e( 'Data empty. No timeline entries were found.' ); ?></p>
+                        <!-- end #portfolio-well --></div>
+                    <?php endif; ?>
+
         <?php
         return ob_get_clean();
 
@@ -40,7 +76,14 @@
         ob_start();
         ?>
 
-                        
+            <div class="game-timeline-list">
+
+                <div class="game" id="<?php ?>">
+
+                <!-- end .game --></div>
+
+            <!-- end .game-timeline-list --></div>
+
         <?php
         return ob_get_clean();
 
@@ -64,6 +107,11 @@
         ob_start();
         ?>
 
+            <ol class="carousel-indicators">
+
+                <li data-target="#carouselExampleIndicators"></li>
+
+            <!-- end .carousel-indicators --></ol>
                         
         <?php
         return ob_get_clean();
