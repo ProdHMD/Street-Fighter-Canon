07d90cbcf8caa37b92f7f7b2b4427a06c6f0c2e9
lljb3 <lljb3@lljb3.com>
2025-05-03 20:09:26 +0000

    Server merge update

---
+++ web/app/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"j pGvQ0#3-=wK=yfX, $]u^hEU:9>M4<y2@i>8o2)jA*IheNJ2I#P!~6eY2yWu4R";s:7:"version";s:5:"1.1.0";s:13:"attackDataKey";i:791;s:11:"wafDisabled";b:0;}
